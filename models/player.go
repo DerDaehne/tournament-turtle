c7a3cae5b289443fbc3a408cf0b2add8cdad6b6f
David Daehne <david.daehne@ox.io>
2019-03-09 17:56:09 +0100

    Update player.go
    remove bson tags and add ID Parameter

--- models/player.go
+++ models/player.go
@@ -2,8 +2,9 @@ package models
 
 // Player represents a Person who want's to play
 type Player struct {
-	LastName   string `bson:"lastname" json:"lastname"`
-	FirstName  string `bson:"firstname" json:"firstname"`
-	NickName   string `bson:"nickname" json:"nickname"`
-	SkillLevel int    `bson:"skilllevel" json:"skilllevel"`
+	ID         int    `json:"id"`
+	LastName   string `json:"lastname"`
+	FirstName  string `json:"firstname"`
+	NickName   string `json:"nickname"`
+	SkillLevel int    `json:"skilllevel"`
 }
